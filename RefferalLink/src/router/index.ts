9c9feb52e9bf7672539810000144135f60b3de12
NhuY29 <100512487+NhuY29@users.noreply.github.com>
2023-11-01 22:13:25 +0700

    ...

--- RefferalLink/src/router/index.ts
+++ RefferalLink/src/router/index.ts
@@ -5,6 +5,8 @@ import LoginView from '../views/Auth/LoginView.vue'
 import LayoutMenuNgang from '../Layouts/LayoutMenuNgang.vue'
 import UserView from '../views/Auth/UserView.vue'
 import BankView from '../views/Auth/BankView.vue'
+import Campaign from '../views/Auth/Campaign.vue'
+import Province from '../views/Auth/Province.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,7 +30,7 @@ const router = createRouter({
       children: [
         {
           path: '',
-          component: BankView,
+          component:Province ,
         },
         {
           path: 'user',
---
+++ RefferalLink/src/views/Auth/Province.vue
@@ -0,0 +1,28 @@
+<template>
+    <Suspense>
+        <BasicAdminFormVue :tableColumns="tableColumns" :apiName="'Province'" :allowAdd="true" :allowEdit="true"
+            :allowDelete="true" title="Province" :CustomActions="CustomActions" ></BasicAdminFormVue>
+    </Suspense>
+</template>
+<script lang="ts" setup>
+import { TableColumn } from '@/components/maynghien/adminTable/Models/TableColumn'
+import BasicAdminFormVue from '@/components/maynghien/adminTable/BasicAdminForm.vue'
+import {CustomAction} from '@/components/maynghien/adminTable/Models/CustomAction'
+const tableColumns: TableColumn[] = [
+    {
+        key: "name",
+        label: "Tên Tỉnh",
+        width: 1000,
+        sortable: true,
+        enableEdit:  true,
+
+        enableCreate:  true,
+        required:false,
+        hidden: false,
+        showSearch: true,
+        inputType:  "text",
+        dropdownData:null,
+    },
+]
+const CustomActions: CustomAction[]=([]);
+</script>
