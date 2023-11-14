2101ffca73d9ab0b754436e196d71ef7563ea9d8
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-11-14 19:51:36 +0700

    ssdf

--- RefferalLink/src/views/Auth/HomePageView.vue
+++ RefferalLink/src/views/Auth/HomePageView.vue
@@ -1,3 +1,17 @@
 <template>
     <p>Hello </p>
 </template>
+<script setup lang="ts">
+import { decode } from 'jsonwebtoken';
+import Cookies from 'js-cookie';
+
+
+
+
+function a (){
+    var token = Cookies.get('accessToken')?.toString();
+    const decodedToken = decode(token ?? '');
+    console.log(decodedToken);
+}
+a();
+</script>
