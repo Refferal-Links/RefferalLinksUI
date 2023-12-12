7ab4233f756aeb0ef9329a9e40ae3fc1605112ea
binh.phan@s3corp.com.vn <binh.phan@s3corp.com.vn>
2023-12-12 15:16:35 +0700

    sdfsdfsd

--- RefferalLink/vite.config.ts
+++ RefferalLink/vite.config.ts
@@ -1,26 +1,14 @@
-
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-
-export default ({ mode}:{mode:any}) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return defineConfig({
-    define: {
-      'process.env': env,
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-    plugins: [vue()],
-    server: {
-      host: true,
-      port: 8080,
-    },
-    resolve: {
-      alias: {
-        // Assuming the "src" directory is in the root of your project
-        '@': '/src',
-      },
-    },
-  });
-};
+  },
+  plugins: [
+    vue()
+  ]
+});
